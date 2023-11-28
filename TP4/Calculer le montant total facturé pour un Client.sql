19f7050f8fb50a2868d53543400f161797a48bb7
naiso-a <martin.khyari@gmail.com>
2023-11-28 11:17:08 +0100

    modification fichier sql

--- TP4/Calculer le montant total facturé pour un Client.sql
+++ TP4/Calculer le montant total facturé pour un Client.sql
@@ -7,4 +7,4 @@ LEFT JOIN projet p ON c.id = p.client_id
 LEFT JOIN devis d ON p.id = d.projet_id
 LEFT JOIN facture f ON d.id = f.devis_id
 WHERE c.id = 1
-GROUP BY c.id, c.nom; -- Ajout de la clause GROUP BY
+GROUP BY c.id, c.nom;
